26743c794640127b16c5df4c3f91c75eea5cc716
WhorTez <dortiz1@knights.ucf.edu>
2021-09-05 21:40:26 -0400

    Initial commit

---
+++ src/main/java/exercise01/solution01.java
@@ -0,0 +1,18 @@
+package exercise01;
+
+import java.util.Scanner;
+
+public class solution01 {
+        /* Ask user for name
+        read users name
+        Output a greeting
+         */
+        public static void main(String[] args){
+            System.out.println("Enter your name: ");
+            Scanner input = new Scanner(System.in);
+            String name = input.nextLine();
+
+            System.out.println("Hello," + name + " hope you have a great day.");
+
+        }
+    }
